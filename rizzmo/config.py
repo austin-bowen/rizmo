bd4b4735a43798306163d2ccce048074a6ee45a2
Austin Bowen <austin.bowen.314@gmail.com>
2024-11-24 15:24:20 -0500

    Fix default camera_index

--- rizzmo/config.py
+++ rizzmo/config.py
@@ -10,7 +10,7 @@ IS_RIZZMO = socket.gethostname() == 'rizzmo'
 class Config:
     coordinator_host: Host = 'rizzmo.local'
 
-    camera_index: int = 11 if IS_RIZZMO else 0
+    camera_index: int = 0
 
 
 config = Config()
