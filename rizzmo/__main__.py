689b2670fd5503c593ac8e81b18476da5c65254e
Austin Bowen <austin.bowen.314@gmail.com>
2024-11-24 15:52:10 -0500

    Set which nodes are started on what host

--- rizzmo/__main__.py
+++ rizzmo/__main__.py
@@ -1,17 +1,36 @@
+import socket
 from time import sleep
 
+from rizzmo.config import IS_RIZZMO
 from rizzmo.procman import ProcessManager
+
+host_nodes = {
+    'rizzmo': (
+        'camera_node',
+        'mic_node',
+        'website_node',
+    ),
+    'potato': (
+        'objrec_node',
+        'vad_node',
+    ),
+    'austin-laptop': (
+        'display_objs_node',
+        'audio_viz_node',
+    )
+}
 
 
 def main():
-    with ProcessManager() as p:
-        p.start_python_module('easymesh.coordinator')
-        sleep(1)
+    host = socket.gethostname()
+    nodes_to_start = host_nodes[host]
 
-        for node in (
-                'mic_node',
-                'audio_viz_node',
-        ):
+    with ProcessManager() as p:
+        if IS_RIZZMO:
+            p.start_python_module('easymesh.coordinator')
+            sleep(1)
+
+        for node in nodes_to_start:
             p.start_python_module(f'rizzmo.nodes.{node}')
 
         try:
