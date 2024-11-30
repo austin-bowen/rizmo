cc962422afc79095a000184ecee5a7084ea279ff
Austin Bowen <austin.bowen.314@gmail.com>
2024-11-30 00:33:44 -0500

    Add command processor node

---
+++ rizzmo/nodes/cmd_proc.py
@@ -0,0 +1,108 @@
+import asyncio
+import subprocess
+from argparse import Namespace
+from collections.abc import Iterable
+from datetime import datetime
+
+from easymesh import build_mesh_node_from_args
+from easymesh.argparse import get_node_arg_parser
+from easymesh.asyncio import forever
+
+from rizzmo.config import config
+
+
+async def main(args: Namespace) -> None:
+    node = await build_mesh_node_from_args(args=args)
+
+    async def handle_transcript(topic, transcript: str) -> None:
+        print()
+        print(f'Transcript: {transcript}')
+
+        transcript = preprocess(transcript)
+
+        if not talking_to_rizzmo(transcript):
+            print('[Not talking to me.]')
+            return
+
+        if any_phrase_in(transcript, (
+                'shut down now',
+                'shutdown now',
+                'rest in a deep and dreamless slumber',
+        )):
+            await shutdown()
+        elif any_phrase_in(transcript, (
+                'what is today',
+                "what is today's date",
+                "what's today",
+                'what day is it',
+        )):
+            await say_date()
+        elif any_phrase_in(transcript, (
+                'what time is it',
+                "what's the time",
+        )):
+            await say_time()
+
+    async def shutdown() -> None:
+        await say('Shutting down.')
+        await asyncio.sleep(3)
+        subprocess.run(['sudo', 'poweroff'])
+
+    async def say_date() -> None:
+        current_date = datetime.now()
+        current_date = current_date.strftime('%A, %B %d')
+
+        await say(f'It is {current_date}.')
+
+    async def say_time() -> None:
+        current_time = datetime.now()
+        current_time = current_time.strftime('%I:%M %p')
+
+        await say(f'It is {current_time}.')
+
+    async def say(message: str) -> None:
+        print(repr(message))
+        await node.send('say', message)
+
+    await node.listen('transcript', handle_transcript)
+    await forever()
+
+
+def preprocess(transcript: str) -> str:
+    t = transcript.lower()
+
+    for alt_name in [
+        'rizmo',
+        'prizmo',
+        'rizma',
+    ]:
+        t.replace(alt_name, 'rizzmo')
+
+    return t
+
+
+def talking_to_rizzmo(transcript: str) -> bool:
+    return any_phrase_in(transcript, (
+        'hey rizzmo',
+        'hi rizzmo',
+        'hello rizzmo',
+        'okay rizzmo',
+        'ok rizzmo',
+    ))
+
+
+def any_phrase_in(transcript: str, phrases: Iterable[str]) -> bool:
+    return any(phrase in transcript for phrase in phrases)
+
+
+def parse_args() -> Namespace:
+    parser = get_node_arg_parser(
+        default_node_name='cmd_proc',
+        default_coordinator=config.coordinator,
+    )
+
+    return parser.parse_args()
+
+
+if __name__ == '__main__':
+    asyncio.run(main(parse_args()))
