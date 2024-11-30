e8067c750b88d9589c61b301fe1cdb1719e516d1
Austin Bowen <austin.bowen.314@gmail.com>
2024-11-29 20:02:18 -0500

    Add asr.py

---
+++ rizzmo/nodes/asr.py
@@ -0,0 +1,124 @@
+import asyncio
+from collections.abc import Callable
+from queue import Queue
+from threading import Thread
+
+import numpy as np
+import torch
+from easymesh import build_mesh_node_from_args
+from easymesh.asyncio import forever
+from transformers import AutoModelForSpeechSeq2Seq, AutoProcessor, pipeline
+
+from rizzmo.nodes.messages import Audio
+
+
+class ASR(Thread):
+    def __init__(
+            self,
+            pipe,
+            handle_transcript: Callable[[str], None],
+            name='ASR',
+            daemon=True,
+            **kwargs,
+    ):
+        super().__init__(name=name, daemon=daemon, **kwargs)
+
+        self.pipe = pipe
+        self.handle_transcript = handle_transcript
+        self.queue = Queue()
+
+    def run(self) -> None:
+        while (audio := self.queue.get()) is not None:
+            sample = dict(
+                array=audio.data.copy(),
+                sampling_rate=audio.sample_rate,
+            )
+
+            result = self.pipe(
+                sample,
+                generate_kwargs={'language': 'english'},
+            )
+
+            transcript = result['text']
+
+            self.handle_transcript(transcript)
+
+    def stop(self):
+        self.queue.put(None)
+
+
+def build_asr_thread(handle_transcript: Callable[[str], None]):
+    device = 'cuda:0' if torch.cuda.is_available() else 'cpu'
+    print(f'Device: {device}')
+
+    torch_dtype = torch.float16 if torch.cuda.is_available() else torch.float32
+
+    model_id = 'openai/whisper-large-v3-turbo'
+
+    model = AutoModelForSpeechSeq2Seq.from_pretrained(
+        model_id,
+        torch_dtype=torch_dtype,
+        low_cpu_mem_usage=True,
+        use_safetensors=True,
+    ).to(device)
+
+    processor = AutoProcessor.from_pretrained(model_id)
+
+    pipe = pipeline(
+        'automatic-speech-recognition',
+        model=model,
+        tokenizer=processor.tokenizer,
+        feature_extractor=processor.feature_extractor,
+        device=device,
+        torch_dtype=torch_dtype,
+        chunk_length_s=30,
+    )
+
+    asr = ASR(pipe, handle_transcript)
+    asr.start()
+    return asr
+
+
+async def main():
+    node = await build_mesh_node_from_args(default_node_name='asr')
+    transcript_topic = node.get_topic_sender('transcript')
+
+    loop = asyncio.get_event_loop()
+
+    def handle_transcript(transcript: str) -> None:
+        print(f'Transcript: {transcript}')
+
+        asyncio.run_coroutine_threadsafe(
+            transcript_topic.send(transcript),
+            loop,
+        ).result()
+
+    asr = build_asr_thread(handle_transcript)
+
+    audio_buffer = []
+
+    async def handle_voice_detected(topic, data):
+        audio, timestamp, voice_detected = data
+
+        if voice_detected:
+            if not audio_buffer:
+                print('Voice started')
+
+            audio_buffer.append(audio)
+            return
+
+        if audio_buffer:
+            print('Voice stopped')
+            full_audio_data = np.concatenate([a.data for a in audio_buffer]).flatten()
+            sample_rate = audio_buffer[0].sample_rate
+            audio_buffer.clear()
+
+            full_audio = Audio(full_audio_data, sample_rate)
+            asr.queue.put(full_audio)
+
+    await node.listen('voice_detected', handle_voice_detected)
+    await forever()
+
+
+if __name__ == '__main__':
+    asyncio.run(main())
