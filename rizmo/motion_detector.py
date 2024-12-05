8d4d19d797392cbd5f15902313ff6433e0369f73
Austin Bowen <austin.bowen.314@gmail.com>
2024-12-04 22:36:27 -0500

    Add dynamic threshold motion detector

--- rizmo/motion_detector.py
+++ rizmo/motion_detector.py
@@ -37,3 +37,35 @@ class PixelChangeMotionDetector(MotionDetector):
         self.prev_image = image
 
         return self.diff > self.threshold
+
+
+class PixelChangeMotionDetectorDynamicThreshold(MotionDetector):
+    def __init__(self, change: float, subsample: int = 1, alpha: float = 0.1):
+        require(0 <= change <= 1, f'Change must be between 0 and 1; got {change}')
+        require(subsample >= 1, f'Subsample must be at least 1; got {subsample}')
+
+        self.change = change
+        self.subsample = subsample
+        self.alpha = alpha
+
+        self.prev_image = None
+        self.diff = 0.
+        self.avg_diff = 0.
+
+    def is_motion(self, image: Image) -> bool:
+        image = image[::self.subsample, ::self.subsample]
+        image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+
+        if self.prev_image is None:
+            self.prev_image = image
+            return False
+
+        self.diff = np.abs(image.astype(float) - self.prev_image)
+        self.diff = float(np.mean(self.diff)) / 255
+
+        is_motion = self.diff / self.avg_diff > self.change
+
+        self.prev_image = image
+        self.avg_diff = self.alpha * self.diff + (1 - self.alpha) * self.avg_diff
+
+        return is_motion
