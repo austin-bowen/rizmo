62c4a93d74737a408c64cbd91c502da3cf8163a5
Austin Bowen <austin.bowen.314@gmail.com>
2024-12-08 14:44:10 -0500

    Add template node

---
+++ rizmo/nodes/template.py
@@ -0,0 +1,21 @@
+import asyncio
+from argparse import Namespace
+
+from easymesh import build_mesh_node_from_args
+
+from rizmo.node_args import get_rizmo_node_arg_parser
+from rizmo.signal import graceful_shutdown_on_sigterm
+
+
+async def main(args: Namespace) -> None:
+    node = await build_mesh_node_from_args(args=args)
+
+
+def parse_args() -> Namespace:
+    parser = get_rizmo_node_arg_parser('TODO')
+    return parser.parse_args()
+
+
+if __name__ == '__main__':
+    graceful_shutdown_on_sigterm()
+    asyncio.run(main(parse_args()))
