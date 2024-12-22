057e9b12557f29b5ff6af18da2a3289876208b49
Austin Bowen <austin.bowen.314@gmail.com>
2024-12-22 14:33:09 -0500

    Create RPC server & client for running Python 3.6 code

---
+++ rizmo/py36/client.py
@@ -0,0 +1,104 @@
+"""Client for the Python 3.6 server running on the Jetson Nano."""
+
+import code
+import pickle
+import socket
+from typing import Any
+
+from typing_extensions import Callable
+
+from rizmo.py36.server import DEFAULT_SOCKET_PATH, PICKLE_PROTOCOL, read_message, write_message
+
+
+class Client:
+    def __init__(
+            self,
+            conn_builder: Callable[[], 'Connection'],
+    ) -> None:
+        self.conn_builder = conn_builder
+
+        self._conn = None
+
+    def __enter__(self):
+        return self
+
+    def __exit__(self, exc_type, exc_value, traceback):
+        self.close()
+
+    @classmethod
+    def build(
+            cls,
+            socket_path: str = DEFAULT_SOCKET_PATH,
+    ) -> 'Client':
+        def conn_builder():
+            sock = socket.socket(socket.AF_UNIX, socket.SOCK_STREAM)
+            sock.connect(socket_path)
+            return Connection(sock)
+
+        return cls(conn_builder)
+
+    @property
+    def conn(self) -> 'Connection':
+        if self._conn is None:
+            self._conn = self.conn_builder()
+
+        return self._conn
+
+    def close(self) -> None:
+        if self._conn is not None:
+            self._conn.close()
+            self._conn = None
+
+    def ping(self) -> str:
+        return self.rpc('ping')
+
+    def stop_server(self) -> None:
+        return self.rpc('stop_server')
+
+    def rpc(self, function, *args, **kwargs) -> Any:
+        try:
+            self._send_rpc_request((function, args, kwargs))
+            return self._read_rpc_response()
+        except BrokenPipeError:
+            self.close()
+            raise
+
+    def _send_rpc_request(self, rpc_request):
+        request_data = pickle.dumps(rpc_request, protocol=PICKLE_PROTOCOL)
+        write_message(self.conn.wfile, request_data)
+
+    def _read_rpc_response(self) -> Any:
+        response_data = read_message(self.conn.rfile)
+        return pickle.loads(response_data)
+
+
+class Connection:
+    def __init__(
+            self,
+            sock: socket.socket,
+            rbufsize: int = -1,
+            wbufsize: int = 0,
+    ) -> None:
+        self.sock = sock
+        self.rfile = sock.makefile('rb', buffering=rbufsize)
+        self.wfile = sock.makefile('wb', buffering=wbufsize)
+
+    def __enter__(self):
+        return self
+
+    def __exit__(self, exc_type, exc_value, traceback):
+        self.close()
+
+    def close(self) -> None:
+        self.sock.close()
+
+
+def main() -> None:
+    client = Client.build()
+
+    print('Use variable `client` to interact with the server.')
+    code.interact(local=locals())
+
+
+if __name__ == '__main__':
+    main()
---
+++ rizmo/py36/server.py
@@ -0,0 +1,157 @@
+"""
+Server for running code on the Jetson Nano that requires the native Python 3.6.
+"""
+
+import os
+import pickle
+import signal
+import sys
+import time
+from argparse import ArgumentParser, Namespace
+from socketserver import StreamRequestHandler, UnixStreamServer
+from threading import Thread
+from typing import Any
+
+from rizmo.signal import graceful_shutdown_on_sigterm
+
+try:
+    from jetson.inference import detectNet
+except ImportError:
+    def detectNet(*args, **kwargs):
+        ...
+
+DEFAULT_SOCKET_PATH: str = '/tmp/rizmo.py36_server.sock'
+
+PICKLE_PROTOCOL: int = 4
+"""The highest protocol version supported by Python 3.6."""
+
+
+def main(args: Namespace) -> None:
+    print('Loading model...')
+    model = detectNet(args.network, sys.argv, args.threshold)
+    print('Done.')
+
+    rpc_handler = RpcHandler(model)
+
+    def delete_socket_file():
+        try:
+            os.unlink(args.socket_path)
+        except FileNotFoundError:
+            pass
+
+    delete_socket_file()
+    try:
+        print(f'Starting server at {args.socket_path!r}...')
+        with UnixStreamServer(
+                args.socket_path,
+                RequestHandler.builder(rpc_handler),
+        ) as server:
+            rpc_handler.server = server
+            server.serve_forever()
+    finally:
+        delete_socket_file()
+
+
+class RpcHandler:
+    def __init__(self, model: detectNet):
+        self.model = model
+        self.server = None
+
+    def __call__(self, function_name, args, kwargs) -> Any:
+        function = getattr(self, function_name)
+        return function(*args, **kwargs)
+
+    def detect(self, image: bytes) -> Any:
+        return self.model.Detect(image)
+
+    def ping(self) -> str:
+        return 'pong'
+
+    def stop_server(self) -> None:
+        print('Stopping server...')
+
+        def stop():
+            time.sleep(0.5)
+            os.kill(os.getpid(), signal.SIGINT)
+
+        Thread(target=stop, daemon=True).start()
+
+
+class RequestHandler(StreamRequestHandler):
+    def __init__(self, rpc_handler: RpcHandler, *args):
+        self.rpc_handler = rpc_handler
+        super().__init__(*args)
+
+    @classmethod
+    def builder(cls, model):
+        def builder(*args) -> RequestHandler:
+            return cls(model, *args)
+
+        return builder
+
+    def handle(self) -> None:
+        try:
+            while True:
+                self._handle_one_request()
+        except EOFError:
+            pass
+
+    def _handle_one_request(self) -> None:
+        request = self._get_rpc_request()
+        response = self.rpc_handler(*request)
+        self._write_response(response)
+
+    def _get_rpc_request(self):
+        request_data = read_message(self.rfile)
+        return pickle.loads(request_data)
+
+    def _write_response(self, response: Any) -> None:
+        response_data = pickle.dumps(response, protocol=PICKLE_PROTOCOL)
+        write_message(self.wfile, response_data)
+
+
+def read_message(rfile) -> bytes:
+    message_len = int.from_bytes(
+        rfile.read(4),
+        byteorder='big',
+        signed=False,
+    )
+
+    return rfile.read(message_len)
+
+
+def write_message(wfile, message: bytes) -> None:
+    message_len = len(message).to_bytes(4, byteorder='big', signed=False)
+    wfile.write(message_len)
+    wfile.write(message)
+    wfile.flush()
+
+
+def parse_args() -> Namespace:
+    parser = ArgumentParser()
+
+    parser.add_argument(
+        '--socket-path',
+        default=DEFAULT_SOCKET_PATH,
+        help='Path to the Unix socket. Default: %(default)s',
+    )
+
+    parser.add_argument(
+        '--network',
+        default='ssd-mobilenet-v2',
+        help='Pre-trained model to load. Default: %(default)s',
+    )
+
+    parser.add_argument(
+        '--threshold',
+        type=float,
+        default=0.5,
+        help='Minimum detection threshold to use. Default: %(default)s',
+    )
+
+    return parser.parse_args()
+
+
+if __name__ == '__main__':
+    graceful_shutdown_on_sigterm()
+    main(parse_args())
