6deb589e80b7edd37a46e12d71e06f3e302e709e
Austin Bowen <austin.bowen.314@gmail.com>
2024-12-29 17:07:37 -0500

    Add TODO

--- rizmo/__main__.py
+++ rizmo/__main__.py
@@ -65,6 +65,7 @@ def main(args: Namespace):
             # Have to use shell mode to redirect output to a file
             with p.options(shell=True):
                 for _ in range(node.count):
+                    # TODO redirect stderr to log as well
                     p.start_python(
                         '-u', '-m', f'rizmo.nodes.{node.name}',
                         *node.args,
