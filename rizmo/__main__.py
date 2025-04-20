e9ef204b04ed8d72c61602094e1b6a8672c02069
Austin Bowen <austin.bowen.314@gmail.com>
2025-04-20 19:08:15 -0400

    Run easymesh command directly

--- rizmo/__main__.py
+++ rizmo/__main__.py
@@ -49,8 +49,8 @@ def main(args: Namespace):
 
     with ProcessManager() as p:
         if IS_RIZMO:
-            p.start_python(
-                '-m', 'easymesh',
+            p.popen(
+                'easymesh',
                 '--authkey', config.mesh_authkey,
             )
 
