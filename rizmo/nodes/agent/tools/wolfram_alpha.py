1236fb6fa8709d119c904b7f7c3e78f4ecfc7457
Austin Bowen <austin.bowen.314@gmail.com>
2025-06-21 13:59:56 -0400

    Improve wolframalpha tool

--- rizmo/nodes/agent/tools/wolfram_alpha.py
+++ rizmo/nodes/agent/tools/wolfram_alpha.py
@@ -1,3 +1,5 @@
+from typing import Optional
+
 import wolframalpha
 
 from rizmo.llm_utils import Tool
@@ -30,4 +32,29 @@ class WolframAlphaTool(Tool):
 
     async def call(self, query: str) -> str:
         response = await self.client.aquery(query)
-        return next(response.results).text
+
+        result = self._get_result(response)
+        if result:
+            return result
+
+        did_you_mean = self._get_did_you_mean(response)
+        if did_you_mean:
+            return f'Did you mean: {did_you_mean}'
+
+        print(f'ERROR response: {response}')
+        return 'ERROR: No response.'
+
+    def _get_result(self, response: wolframalpha.Result) -> Optional[str]:
+        result = next(response.results, None)
+        return result.text if result else None
+
+    def _get_did_you_mean(self, response: wolframalpha.Result) -> Optional[str]:
+        did_you_mean = response.get('didyoumeans', None)
+        if not did_you_mean:
+            return None
+
+        did_you_mean = did_you_mean.get('didyoumean', None)
+        if not did_you_mean:
+            return None
+
+        return did_you_mean.get('@val', None)
